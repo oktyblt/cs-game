61812ec87e1878613a4a321ea2e42817beb5d443
Oktay <tusevonline@gmail.com>
2026-07-11 00:31:09 +0300

    chore: bump WASM to v40, v68, v39, disable DamageInfo, add debug flags

--- cs-web-game/src/main.js
+++ cs-web-game/src/main.js
@@ -1559,9 +1559,9 @@ async function initEngine(mapName, connectPort = null, isHost = false) {
     ] = await Promise.all([
       cachedFetch(`${ASSET_URL}/cs-assets/valve/gfx.wad`),
       cachedFetch(`${ASSET_URL}/cs-assets/valve/fonts.wad`),
-      cachedFetch('/wasm/dlls/cs_emscripten_wasm32_v39.wasm'),
-      cachedFetch('/wasm/cl_dlls/client_emscripten_wasm32_v67.wasm'),
-      cachedFetch('/wasm/cl_dlls/menu_emscripten_wasm32_v38.wasm'),
+      cachedFetch('/wasm/dlls/cs_emscripten_wasm32_v40.wasm'),
+      cachedFetch('/wasm/cl_dlls/client_emscripten_wasm32_v68.wasm'),
+      cachedFetch('/wasm/cl_dlls/menu_emscripten_wasm32_v39.wasm'),
       cachedFetch('/wasm/filesystem_stdio.wasm'),
       cachedFetch('/wasm/libref_webgl2.wasm'),
       cachedFetch(`${ASSET_URL}/cs-assets/valve/delta.lst`),
@@ -1730,9 +1730,9 @@ async function initEngine(mapName, connectPort = null, isHost = false) {
       },
 
       libraries: {
-        menu: '/wasm/cl_dlls/menu_emscripten_wasm32_v38.wasm',
-        client: '/wasm/cl_dlls/client_emscripten_wasm32_v67.wasm',
-        server: '/wasm/dlls/cs_emscripten_wasm32_v39.wasm',
+        menu: '/wasm/cl_dlls/menu_emscripten_wasm32_v39.wasm',
+        client: '/wasm/cl_dlls/client_emscripten_wasm32_v68.wasm',
+        server: '/wasm/dlls/cs_emscripten_wasm32_v40.wasm',
         render: {
           gl4es: '/wasm/libref_webgl2.wasm'
         }
@@ -1740,9 +1740,9 @@ async function initEngine(mapName, connectPort = null, isHost = false) {
 
       filesMap: {
         'filesystem_stdio.wasm': '/wasm/filesystem_stdio.wasm',
-        'cl_dlls/menu_emscripten_wasm32.wasm': '/wasm/cl_dlls/menu_emscripten_wasm32_v38.wasm',
-        'cl_dlls/client_emscripten_wasm32.wasm': '/wasm/cl_dlls/client_emscripten_wasm32_v67.wasm',
-        'dlls/cs_emscripten_wasm32.wasm': '/wasm/dlls/cs_emscripten_wasm32_v39.wasm',
+        'cl_dlls/menu_emscripten_wasm32.wasm': '/wasm/cl_dlls/menu_emscripten_wasm32_v39.wasm',
+        'cl_dlls/client_emscripten_wasm32.wasm': '/wasm/cl_dlls/client_emscripten_wasm32_v68.wasm',
+        'dlls/cs_emscripten_wasm32.wasm': '/wasm/dlls/cs_emscripten_wasm32_v40.wasm',
         'dlls/hl_emscripten_wasm32.wasm': '/wasm/dlls/cs_emscripten_wasm32_v21.wasm',
       },
 
