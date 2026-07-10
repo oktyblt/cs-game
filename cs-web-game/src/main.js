4eda9943fc1db211604d43c35974462f27beaa2c
Oktay <tusevonline@gmail.com>
2026-07-10 05:28:58 +0300

    fix: restore unversioned virtual filesystem paths in main.js

--- cs-web-game/src/main.js
+++ cs-web-game/src/main.js
@@ -1766,7 +1766,7 @@ async function initEngine(mapName, connectPort = null, isHost = false) {
       filesMap: {
         'filesystem_stdio.wasm': '/wasm/filesystem_stdio.wasm',
         'cl_dlls/menu_emscripten_wasm32.wasm': '/wasm/cl_dlls/menu_emscripten_wasm32_v34.wasm',
-        'cl_dlls/client_emscripten_wasm32_v34.wasm': '/wasm/cl_dlls/client_emscripten_wasm32_v34.wasm',
+        'cl_dlls/client_emscripten_wasm32.wasm': '/wasm/cl_dlls/client_emscripten_wasm32_v34.wasm',
         'dlls/cs_emscripten_wasm32.wasm': '/wasm/dlls/cs_emscripten_wasm32_v34.wasm',
         'dlls/hl_emscripten_wasm32.wasm': '/wasm/dlls/cs_emscripten_wasm32_v34.wasm',
       },
@@ -2048,7 +2048,7 @@ async function initEngine(mapName, connectPort = null, isHost = false) {
 
           // DLL dosyaları
           em.FS.writeFile('/cstrike/dlls/cs_emscripten_wasm32.wasm', csServerBuffer);
-          em.FS.writeFile('/cstrike/cl_dlls/client_emscripten_wasm32_v34.wasm', csClientBuffer);
+          em.FS.writeFile('/cstrike/cl_dlls/client_emscripten_wasm32.wasm', csClientBuffer);
           em.FS.writeFile('/cstrike/cl_dlls/menu_emscripten_wasm32.wasm', csMenuBuffer);
 
           em.FS.writeFile('/filesystem_stdio.wasm', fsBuffer);
